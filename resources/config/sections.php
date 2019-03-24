cb6409861714b2c31129ab2d47d524579440b06e
William <william@pixney.com>
2019-03-24 21:20:38 +0100

    .

---
+++ resources/config/sections.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+    'gallery' => [
+        'tabs'    => [
+            'content'     => [
+                'title'  => 'Content',
+                'fields' => [
+                    'entry_headline',
+                    'entry_text',
+                ],
+            ],
+            'link'    => [
+                'title'  => 'Link',
+                'fields' => [
+                    'entry_link_url',
+                    'entry_link_text',
+                ],
+            ],
+            'classes'    => [
+                'title'  => 'Classes',
+                'fields' => [
+                    'entry_classes',
+                ],
+            ]
+        ],
+    ],
+];
