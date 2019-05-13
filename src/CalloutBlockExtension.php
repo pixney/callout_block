9fd6f55e811b593eea5320a4bcc5d068a20de88d
William <william@pixney.com>
2019-05-13 16:25:19 +0200

    content added

--- src/CalloutBlockExtension.php
+++ src/CalloutBlockExtension.php
@@ -10,12 +10,13 @@ class CalloutBlockExtension extends BlockExtension
 {
     protected $provides = 'anomaly.module.blocks::block.callout';
     protected $model    = BlockModel::class;
+    protected $category = 'content';
 
     public function getView()
     {
         $settings      = app(SettingRepositoryInterface::class);
         $setting       = $settings->get('streams::standard_theme');
-        $this->wrapper = $setting->value . '::blocks/global/wrapper';
-        return $setting->value . '::blocks/callout';
+        $this->wrapper = $setting->value . "::blocks/{$this->slug}/wrapper";
+        return $setting->value . "::blocks/{$this->slug}/content";
     }
 }
